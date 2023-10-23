8a8b516c6b968d11026e5af06aeb671098b7b1e2
Zofia Smoleń <zofsmolen@gmail.com>
2023-10-23 11:25:46 +0200

    modifying utils.py and adding saving pickles

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,1 +1,15 @@
 # Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
+from typing import Any
+
+import os
+import pickle
+
+def load_pickle(path: str):
+    with open(path, "rb") as f:
+        loaded_obj = pickle.load(f)
+    return loaded_obj
+
+
+def save_pickle(path: str, obj: Any):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
